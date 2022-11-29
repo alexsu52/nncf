26d60e7eddc26a970930ca32cbda6f14d9dcee73
Alexander Suslov <alexander.suslov@intel.com>
2022-11-29 16:25:44 +0400

    multiprocessing

--- examples/post_training_quantization/torch/mobilenet_v2/main.py
+++ examples/post_training_quantization/torch/mobilenet_v2/main.py
@@ -11,14 +11,13 @@
  limitations under the License.
 """
 
-import os
-import re
-import subprocess
+import multiprocessing
 import sys
 import tempfile
+from multiprocessing import Process
 from pathlib import Path
+import time
 
-import nncf
 import numpy as np
 import openvino.runtime as ov
 import torch
@@ -72,29 +71,6 @@ def ov_convert(model, args):
     return ov_model
 
 
-def ov_benchmark(model, verbose=True):
-    ov_model_path = f'{tempfile.gettempdir()}/model.xml'
-    ov.serialize(model, ov_model_path)
-
-    command = f'benchmark_app -m {ov_model_path} -d CPU -api async -t 15'
-    cmd_output = subprocess.check_output(command, shell=True)
-    if verbose:
-        print(*str(cmd_output).split('\\n')[-9:-1], sep='\n')
-    match = re.search(r"Throughput\: (.+?) FPS", str(cmd_output))
-    return float(match.group(1))
-
-
-def ov_model_size(ir_path, m_type='Mb'):
-    model_size = os.path.getsize(ir_path) + os.path.getsize(ir_path.replace('xml', 'bin'))
-    for t in ['bytes', 'Kb', 'Mb']:
-        if m_type == t:
-            return model_size
-        model_size /= 1024
-    return model_size
-
-###########################################################################
-# Create a PyTorch model and dataset
-###########################################################################
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225])
 val_dataset = datasets.Food101(
@@ -115,65 +91,32 @@ model = models.mobilenet_v2(num_classes=FOOD101_CLASSES)
 model.eval()
 model = load_checkpoint(model)
 
-###########################################################################
-# Quantize a PyTorch model
-###########################################################################
-
-'''
-Transformation function transforms a data item into model input.
-
-To validate the transform function use the following code:
->> for data_item in val_loader:
->>    model(transform_fn(data_item))
-'''
-def transform_fn(data_item):
-    images, _ = data_item
-    return images
-
-calibration_dataset = nncf.Dataset(val_loader, transform_fn)
-quantized_model = nncf.quantize(model, calibration_dataset)
-
-###########################################################################
-# Benchmark performance, calculate compression rate and validate accuracy
-###########################################################################
-
 dummy_input = torch.randn(1, 3, 224, 224)
 ov_model = ov_convert(model.cpu(), dummy_input)
-ov_quantized_model = ov_convert(quantized_model.cpu(), dummy_input)
 
-print('[1/7] Benchmark FP32 model:')
-fp32_fps = ov_benchmark(ov_model, verbose=True)
-print('[2/7] Benchmark INT8 model:')
-int8_fps = ov_benchmark(ov_quantized_model, verbose=True)
-
-fp32_ir_path = f'{ROOT}/mobilenet_v2_fp32.xml'
-int8_ir_path = f'{ROOT}/mobilenet_v2_int8.xml'
-ov.serialize(ov_model, fp32_ir_path)
-ov.serialize(ov_quantized_model, int8_ir_path)
-
-fp32_model_size = ov_model_size(fp32_ir_path)
-int8_model_size = ov_model_size(int8_ir_path)
-print(f'[3/7] Save FP32 model: {fp32_ir_path} ({fp32_model_size:.3f} Mb)')
-print(f'[4/7] Save INT8 model: {int8_ir_path} ({int8_model_size:.3f} Mb)')
-
-# Using [the dynamic shape feature](https://docs.openvino.ai/latest/openvino_docs_OV_UG_DynamicShapes.html)
-# to compute the last batch of the validation dataset
 ov_model.reshape([-1, 3, 224, 224])
-ov_quantized_model.reshape([-1, 3, 224, 224])
 
-compiled_model = ov.compile_model(ov_model)
-compiled_quantized_model = ov.compile_model(ov_quantized_model)
+def fun(ov_model, val_loader, num_threads):
+    config = {}
+    if num_threads > 1:
+        config['AFFINITY'] = 'NUMA'
+        config['INFERENCE_NUM_THREADS'] = multiprocessing.cpu_count() / num_threads
+    compiled_model = ov.Core().compile_model(ov_model, device_name='CPU', config=config)
+    validate(compiled_model, val_loader)
 
-print('[5/7] Validate OpenVINO FP32 model:')
-fp32_top1 = validate(compiled_model, val_loader)
-print(f'Accuracy @ top1: {fp32_top1:.3f}')
+p0 = Process(target=fun, args=(ov_model, val_loader, 1))
+start_time = time.time()
+p0.start()
+p0.join()
+wall_time = time.time() - start_time
+print(f'process time (1 validation): {wall_time:.3f}')
 
-print('[6/7] Validate OpenVINO INT8 model:')
-int8_top1 = validate(compiled_quantized_model, val_loader)
-print(f'Accuracy @ top1: {int8_top1:.3f}')
-
-print('[7/7] Report:')
-print(f'Accuracy drop: {fp32_top1 - int8_top1:.3f}')
-print(f'Compression rate: {fp32_model_size / int8_model_size:.3f}')
-# https://docs.openvino.ai/2018_R5/_docs_IE_DG_Intro_to_Performance.html
-print(f'Performance speed up (throughput mode): {int8_fps / fp32_fps:.3f}') 
+p1 = Process(target=fun, args=(ov_model, val_loader, 2))
+p2 = Process(target=fun, args=(ov_model, val_loader, 2))
+start_time = time.time()
+p1.start()
+p2.start()
+p1.join()
+p2.join()
+wall_time = time.time() - start_time
+print(f'process time (2 parallel validation): {wall_time:.3f}')
